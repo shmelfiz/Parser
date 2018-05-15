d982bb9c79758b622b54cc0024448343a0552c80
shmelfiz <36863984+shmelfiz@users.noreply.github.com>
2018-05-15 22:03:09 +0300

    AddedProject

---
+++ GufoMeParser/Core/Classes/Parser.cs
@@ -0,0 +1,17 @@
+﻿using GufoMeParser.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GufoMeParser.Core.Classes
+{
+    class Parser : IParser
+    {
+        public void Parse()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
